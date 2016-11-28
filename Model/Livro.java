604527d138c9cdaa9a1ae98ddd4fd0c090125fc2
rBBr <reubbers@gmail.com>
2016-11-28 10:22:59 -0300

    Create Livro.java

---
+++ Model/Livro.java
@@ -0,0 +1,44 @@
+
+package Model;
+
+public class Livro {
+    
+    private Long id;
+    private String nome;
+    private String autor;
+    private String sinopse;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+    
+    
+}
